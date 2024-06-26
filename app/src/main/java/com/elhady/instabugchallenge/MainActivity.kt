2f75deaf60cdbfba1ea58c9214f0956a3a763dec
islam elhady <i.elhady44@gmail.com>
2024-06-26 05:59:24 +0300

    add navigation graph

--- app/src/main/java/com/elhady/instabugchallenge/MainActivity.kt
+++ app/src/main/java/com/elhady/instabugchallenge/MainActivity.kt
@@ -5,12 +5,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.elhady.instabugchallenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/elhady/instabugchallenge/TestingViewModel.kt
@@ -0,0 +1,7 @@
+package com.elhady.instabugchallenge
+
+import androidx.lifecycle.ViewModel
+
+class TestingViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
